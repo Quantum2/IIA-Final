3e975c6f5fdaf2acbc1079ca32ce2b01dd4538e2
Quantum2 <rafaelfrancisco6@gmail.com>
2015-01-07 10:38:51 +0000

    Commit #1

---
+++ ConsoleApplication1/Algoritmo.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IIA
+{
+    static class Algoritmo
+    {
+        static List<int> lista;
+        static Random randy;
+
+        public static void TrepaColinas(int[,] mat)
+        {
+            int tempo;
+            fazerFicheiroLog(mat);
+            lista = new List<int>();
+            randy = new Random();
+            Console.Write("Insira o tempo de execução do algoritmo em segundos : ");
+            tempo = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Solução obtida: ");
+            BitArray resultado = new BitArray(TCRunner(mat, tempo).Result);
+            Console.WriteLine("Número de iterações: {0} p/s", lista.Count/tempo);
+            Console.ReadLine();
+        }
+
+        private static void fazerFicheiroLog(int[,] mat)
+        {
+            string path = "log.txt";
+
+            File.Delete(path);
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(path);
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    file.Write(mat[i, j]);
+                    file.Write(' ');
+                }
+                file.Write('\n');
+            }
+
+            file.Close();
+        }
+
+        public static void PrintValues(IEnumerable myList, int myWidth)
+        {
+            int i = myWidth;
+            foreach (Object obj in myList)
+            {
+                if (i <= 0)
+                {
+                    i = myWidth;
+                    Console.WriteLine();
+                }
+                i--;
+                Console.Write("{0,8}", obj);
+            }
+            Console.WriteLine();
+        }
+
+        static async Task<BitArray> TCRunner(int[,] mat, int tempo)
+        {
+            var search = new HillClimber(() => RandomBitArray(mat), Neighbours, Fitness);
+            var optimized = await search.Optimize(TimeSpan.FromSeconds(tempo));
+
+            return optimized;
+        }
+
+        public static BitArray RandomBitArray(int[,] mat)
+        {
+            BitArray b = new BitArray(mat.GetLength(0), false);
+            int rand = randy.Next(0, mat.GetLength(1));
+            lista.Add(rand);
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+                b[i] = Convert.ToBoolean(mat[i, rand]);
+
+            return b;
+        }
+
+        public static IEnumerable<BitArray> Neighbours(BitArray barray)
+        {
+            string[] lines = System.IO.File.ReadAllLines("log.txt");
+            int indice = randy.Next(0, lines.Length);
+            string[] temp = lines[indice].Split(' ');
+            lines = lines.Where((source, index) => index != indice).ToArray();
+            lista.Add(indice);
+
+            for (int w = 0; w < barray.Length; w++)
+            {
+                var n = new BitArray(barray);
+                n[w] = Convert.ToBoolean(Convert.ToInt32(temp[w]));
+
+                yield return n;
+            }
+        }
+
+        public static IEnumerable<bool> ToEnumerable(this BitArray barray)
+        {
+            for (int i = 0; i < barray.Length; i++)
+                yield return barray[i];
+        }
+
+        public static int Fitness(BitArray barray)
+        {
+            return barray.ToEnumerable().Count(b => b);
+        }
+
+        internal static void evo()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void hib()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HillClimber
+    {
+        public Func<BitArray> RandomSolution { get; set; }
+        public Func<BitArray, IEnumerable<BitArray>> Neighbours { get; set; }
+        public Func<BitArray, int> Fitness { get; set; }
+
+        public HillClimber(Func<BitArray> randomSolution, Func<BitArray, IEnumerable<BitArray>> neighbours, Func<BitArray, int> fitness)
+        {
+            this.RandomSolution = randomSolution;
+            this.Neighbours = neighbours;
+            this.Fitness = fitness;
+        }
+
+        public async Task<BitArray> Optimize(TimeSpan timeout)
+        {
+            DateTimeOffset start = DateTimeOffset.Now;
+            BitArray current = this.RandomSolution();
+            int currentFitness = this.Fitness(current);
+
+            return await Task.Factory.StartNew(() =>
+            {
+                do
+                {
+                    BitArray steepestAscentNeighbour = default(BitArray);
+                    int steepestAscentFitness = default(int);
+
+                    foreach (var n in this.Neighbours(current))
+                    {
+                        steepestAscentFitness = this.Fitness(n);
+                        if (Comparer.Default.Compare(steepestAscentFitness, currentFitness) > 0)
+                        {
+                            current = n;
+                            currentFitness = steepestAscentFitness;
+                            foreach (bool bit in current)
+                            {
+                                if(bit == true)
+                                    Console.Write("1");
+                                if (bit == false)
+                                    Console.Write("0");
+                            }
+                            Console.WriteLine(' ');
+                        }
+                    }
+
+                    if (steepestAscentNeighbour != null && Comparer.Default.Compare(currentFitness, steepestAscentFitness) >= 0)
+                        current = steepestAscentNeighbour;
+
+                } while (DateTimeOffset.Now - start < timeout);
+
+                return current;
+            });
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipo;
+            int[,] matriz;
+
+            matriz = gerarMatriz();
+
+            Console.Clear();
+            Console.WriteLine("Escolha o tipo de algoritmo");
+            Console.WriteLine("1 - Trepa Colinas");
+            Console.WriteLine("2 - Algoritmo evolutivo");
+            Console.WriteLine("3 - Método hibrido");
+            tipo = Console.ReadLine();
+
+            switch(Convert.ToInt32(tipo))
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Escolhido Trepa Colinas...");
+                    Algoritmo.TrepaColinas(matriz);
+                    break;
+                case 2:
+                    Algoritmo.evo();
+                    break;
+                case 3:
+                    Algoritmo.hib();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Input errado");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+
+        static int[,] gerarMatriz()
+        {
+            int dim_x, dim_y, contador = 4, contador2 = 0;
+            string filename;
+
+            Console.WriteLine("Escreva o nome do ficheiro de texto : ");
+            filename = Console.ReadLine();
+            Console.Clear();
+
+            string[] lines = System.IO.File.ReadAllLines(filename);
+            string[] temp = lines[0].Split(' ');
+            dim_x = Convert.ToInt32(temp[0]);
+            dim_y = Convert.ToInt32(temp[2]);
+
+            Console.WriteLine("A dimensão X {0} e a Y {1}", dim_y, dim_x);
+
+            int[,] matriz = new int[dim_x,dim_y];
+
+            for (int s = 0; s < matriz.GetLength(0); s++)
+            {
+                string[] numeros_temp = lines[contador + 2].Split('\t');
+
+                for (int i = 0; i < numeros_temp.Length - 1; i++)
+                {
+                    matriz[contador2, Convert.ToInt32(numeros_temp[i]) - 1] = 1;
+                }
+
+                contador2++;
+                contador = contador + 4;
+            }
+
+            Console.WriteLine("A matriz é a seguinte : \n");
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                    Console.Write(" {0}", matriz[i, j].ToString());
+                Console.WriteLine(" ");
+            }
+
+            Console.ReadLine();
+
+            return matriz;
+        }
+    }
+}
